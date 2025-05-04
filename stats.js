ad41b283d64e204d3e979a9a61e47e9b2a7b4f51
cliffamadeus <dev.clfm5t12@gmail.com>
2025-05-04 19:28:56 +0800

    Stat percentage % styling

--- stats.js
+++ stats.js
@@ -1,7 +1,7 @@
 const stats = [
     { label: 'LOOKS', value: 100, color: 'bg-success' },
     { label: 'CODING SKILLS', value: 80, color: 'bg-warning' },
-    { label: 'ENERGY', value: 35, color: 'bg-danger' }
+    { label: 'ENERGY', value: 15, color: 'bg-danger' }
   ];
   
   const statsContainer = document.getElementById('stats-section');
@@ -12,7 +12,7 @@ const stats = [
       <h5 style="margin-top: 2rem; font-family: 'Press Start 2P';">${stat.label}</h5>
       <div class="progress" role="progressbar" aria-label="Basic example" style="height: 40px;">
         <div class="progress-bar ${stat.color} text-white progress-bar-striped progress-bar-animated" style="width: ${stat.value}%;">
-          <b>${stat.value}%</b>
+          <b style="font-size:23px; font-family: 'Press Start 2P';color:#212529;">${stat.value}%</b>
         </div>
       </div>
     `).join('')}
